7c49a3bc55ab59e156b0b3c7765c2ed6362aec4e
Beck2301 <bryang211@hotmail.com>
2024-07-18 15:11:50 -0600

    adding styles and loading icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { FaSpinner } from "react-icons/fa";
 
-const IAimageGeneate = () => {
+const IAimageGenerate = () => {
   const [prompt, setPrompt] = useState<string>("");
   const [imageUrl, setImageUrl] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
   const handleGenerateImage = async () => {
+    setLoading(true);
     try {
       const apiUrl = `/api/generate-image?prompt=${encodeURIComponent(prompt)}`;
       const response = await fetch(apiUrl);
@@ -19,6 +22,8 @@ const IAimageGeneate = () => {
       setImageUrl(imageURL);
     } catch (error) {
       console.error("Error fetching image:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -29,7 +34,7 @@ const IAimageGeneate = () => {
   return (
     <div className="md:max-w-2xl w-3/4">
       <h1 className="text-3xl">Generated Image for Prompt:</h1>
-      <div className="my-5 flex gap-5 justify-center" >
+      <div className="my-5 flex gap-5 justify-center">
         <input
           type="text"
           placeholder="Enter your prompt"
@@ -37,7 +42,16 @@ const IAimageGeneate = () => {
           onChange={handlePromptChange}
           className="bg-transparent border-b w-full"
         />
-        <button onClick={handleGenerateImage} className="bg-white p-3 text-black rounded-lg">Generate</button>
+        <button
+          onClick={handleGenerateImage}
+          className="bg-white p-3 text-black rounded-lg flex items-center"
+          disabled={loading}
+        >
+          {loading && (
+            <FaSpinner className="animate-spin h-5 w-5 mr-2 text-black" />
+          )}
+          {loading ? "Generating" : "Generate"}
+        </button>
       </div>
       {imageUrl && (
         <div>
@@ -46,7 +60,7 @@ const IAimageGeneate = () => {
             src={imageUrl}
             alt="Generated Image"
             style={{ display: "block", userSelect: "none" }}
-            className="w-full mt-5"
+            className="w-full mt-5 rounded-lg"
           />
         </div>
       )}
@@ -54,4 +68,4 @@ const IAimageGeneate = () => {
   );
 };
 
-export default IAimageGeneate;
+export default IAimageGenerate;
